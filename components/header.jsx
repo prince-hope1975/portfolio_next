58e1a768635ca0bf6b8595da1985b45884eefae5
prince-hope1975 <amachreeprince7@gmail.com>
2022-02-22 09:52:40 +0100

    finished home page on mobile

--- components/header.jsx
+++ components/header.jsx
@@ -1,77 +1,102 @@
 import Link from "next/link";
-import { useState } from "react";
-import {useGlobalContext} from "../context"
-const Header = () =>{
-  const [state, setState] = useState(true)
-  const [active, setActive] = useState(0)
-  const handletoggle= ()=>{
-    setState(!state)
-  }
-    return (
-      <header>
-        <div
-          className={`menu-btn ${state ? "" : "close"}`}
-          onClick={handletoggle}
-        >
-          <div className="btn-line"></div>
-          <div className="btn-line"></div>
-          <div className="btn-line"></div>
-        </div>
+import { useState, useRef, useEffect } from "react";
+import { AiFillHome } from "react-icons/ai";
+import { useGlobalContext } from "../context";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-        <nav className={`menu ${state ? "" : "show"}`}>
-          <div className={`menu-branding ${state ? "" : "show"}`}>
-            <div className="portrait"></div>
-          </div>
-          <ul className={`menu-nav ${state ? "" : "show"}`}>
-         
-         {   data.map(({name, link}, index)=>{
-              return <NavItem name={name} link={link} active={active} setActive={setActive} index={index}/>
-            })}
+const Header = () => {
+  const [state, setState] = useState(true);
+  const [active, setActive] = useState(0);
+  const ref = useRef(null);
 
-          </ul>
-        </nav>
-      </header>
-    );
-}
+  // useEffect(() => {
+  //   ref?.current?.active= true
+  // }, [])
 
-const NavItem = ({name, link,state , index}) =>{
-  const {active, setActive} = useGlobalContext()
+  const handletoggle = () => {
+    setState(!state);
+  };
+  return (
+    <header className={`${styles.header}`}>
+      <div class={styles.img}>
+        <Image
+          className={`${styles.image}`}
+          src="/png2.png"
+          width={90}
+          height={100}
+        />
+      </div>
+      <nav className={` ${state ? "" : "show"}`}>
+        <ul className={`${styles["nav-menu"]} ${state ? "" : "show"}`}>
+          {data.map(({ name, link, alt }, index) => {
+            return (
+              <NavItem
+                alt={alt}
+                name={name}
+                link={link}
+                active={active}
+                setActive={setActive}
+                index={index}
+              />
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+const NavItem = ({ name, link, state, index, alt }) => {
+  const { active, setActive } = useGlobalContext();
   return (
     <li
-    key={index}
-      className={`nav-item  ${state ? "" : "show"}  ${
+      key={index}
+      className={`${styles["nav-item"]}  ${state ? "" : "show"}  ${
         active === index ? "current" : ""
       }`}
-      onClick={async()=>{
-       await setActive(index)
-        console.log( active)
-      }
-      }
+      onClick={() => {
+        setActive(index);
+        console.log(active);
+      }}
     >
       <Link href={`/${link}`}>
-        <a className="nav-link">{name || "Home"}</a>
+        {!alt ? (
+          <a className={`${styles["home-link"]}`}>{name || "Home"}</a>
+        ) : (
+          <a className={`${styles["home-link"]}`}>
+            <span className={styles.homeText}>Home</span>
+            <span
+              className={`${styles.homeSvg} ${
+                active === 0 && styles.invisible
+              }`}
+            >
+              {alt}
+            </span>
+          </a>
+        )}
       </Link>
     </li>
   );
-}
-
+};
 
 const data = [
   {
     name: "Home",
+    alt: <AiFillHome />,
     link: "",
   },
   {
-    name: "About Me",
+    name: "About",
     link: "about",
   },
   {
-    name: "My Work",
+    name: "Portfolio",
     link: "projects",
   },
   {
-    name: "How To Reach ME",
+    name: "Contact me",
     link: "contact",
   },
 ];
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-import Head from 'next/head'
-import Header from '../components/header';
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/header";
+import styles from "../styles/Home.module.css";
+import {
+  AiOutlineTwitter,
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillMail,
+} from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -10,12 +18,43 @@ export default function Home() {
           name="Home page"
           content="Prince charles Amachree Portfoliio Homepage"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/png.ico" />
       </Head>
       <Header />
-      <body >
-        
-      </body>
+
+      <div className={`${styles.squiggle}`}>
+        <Image src="/Saly-30.png" width={750} height={300} />
+      </div>
+      <main className={`${styles.main}`}>
+        <div className={`${styles.logo}`}>
+          <Image className={``} src="/png2.png" width={130} height={140} />
+        </div>
+
+        <div className={`${styles.text}`}>
+          <h1>
+            Hey <br /> there !
+          </h1>
+          <p>
+            I'm Prince Charles <br />
+            {"<" + "Software Developer" + "/>"}
+          </p>
+        </div>
+        <div className={`${styles.lolipop}`}>
+          <Image className={``} src="/Saly-31.png" width={450} height={180} />
+        </div>
+      </main>
+      <footer className={`${styles.footer}`}>
+        <div className={styles.contacts}>
+          <AiFillLinkedin />
+          <AiFillGithub />
+          <AiFillMail />
+          <AiOutlineTwitter />
+        </div>
+
+        <div className={`${styles.end}`}>
+          <Image src="/Saly-33.png" width={1600} height={800} />
+        </div>
+      </footer>
     </>
   );
 }
